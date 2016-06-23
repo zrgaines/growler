ff3eaf4d1fce6b1426ceb1071004aa7de693b040
Zach Gaines <zrgaines@mtu.edu>
2016-06-23 14:48:31 -0500

    stuff works

--- models/growl.js
+++ models/growl.js
@@ -4,12 +4,6 @@ function Growl(options) {
   Growl.all.push(this);
 }
 
-
-Growl.find = function(id) {
-  return Growl.all.find(function(product) {
-    return growl.id === id;
-  });
-}
 Growl.all = [];
 
 module.exports = Growl;
