c8df56f997ebd6969e05b76ed8b1270f8fecde65
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-06-23 10:42:41 -0500

    routes sketched out index.js, add routes/growls.js

---
+++ routes/growls.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var session = require('express-session');
+
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+
+// });
+
+
