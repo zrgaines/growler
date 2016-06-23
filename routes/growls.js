0e6a98f00344d4fa4fabc99c905b904efd35b8b7
Zach Gaines <zrgaines@mtu.edu>
2016-06-23 11:00:12 -0500

    progress

--- routes/growls.js
+++ routes/growls.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var session = require('express-session');
+var Growl = require('../models/growl')
 
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 
 // });
-
-
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var session = require('express-session');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Growler' });
+  console.log(req.session)
 });
 
 
@@ -15,8 +16,10 @@ router.get('/', function(req, res, next) {
 // });
 
 
-// router.post('/growls', function(req, res, next) {
-
+// router.post('/', function(req, res, next) {
+//   var growl = Growl.all;
+//   req.session.feed.push(growl);
+//   console.log(growl);
 //   res.redirect('/');
 // });
 
